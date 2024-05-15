26e75d71249aa314fbb11d81ed6b3d007c486a77
David Aranda <david.aranda@gmail.com>
2024-05-15 13:34:42 +0200

    Arreglando las funciones para borrar y editar productos

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -103,12 +103,16 @@ const updateProduct = async (req, res) => {
   }
 
   try {
-    const existingProduct = await productModel.findById(id);
-    if (!existingProduct) {
+    const product = await productModel.findById(id);
+    if (!product) {
       return res.status(404).json({ error: "Product not found" });
     }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 
-    await Product.findByIdAndUpdate(id, req.body);
+  try {
+    await productModel.findByIdAndUpdate(id, req.body);
 
     res.status(200).json({ data: "Product updated" });
   } catch (error) {
