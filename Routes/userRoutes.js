4aea769045ac45cb388546f397329ce23f231a43
Cristian <cristiangarciaibanez95@gmail.com>
2024-05-17 19:48:21 +0200

    commit

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -10,17 +10,12 @@ const UsersRouter = express.Router();
 
 UsersRouter.post("/register", registerUser);
 UsersRouter.post("/login", loginUser);
-<<<<<<< HEAD
-//UsersRouter.post("/forgot", verifyToken, isAdmin, forgotUser);
-//UsersRouter.get("/:user_id", verifyToken, showDashboard);
-//UsersRouter.put("/:user_id", verifyToken, editUserData);
-//UsersRouter.delete("/:user_id", verifyToken, isAdmin, deleteUser);
-=======
 UsersRouter.use(verifyToken);
+
+//UsersRouter.delete("/:user_id", isAdmin, deleteUser);
 //UsersRouter.post("/forgot", isAdmin, forgotUser);
 //UsersRouter.get("/:user_id", showDashboard);
 //UsersRouter.put("/:user_id", editUserData);
->>>>>>> 63c739af6b32d227571df3535459284fb16001da
 
 module.exports = {
   UsersRouter,
