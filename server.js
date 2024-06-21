63e5dcf5d30bf56a7dfb5859ce410b6c6bdda63d
David Aranda <david.aranda@gmail.com>
2024-06-21 21:33:43 +0200

    añadiendo ruta get /

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
+app.use("/", ProductsRouter);
 app.use("/users", UsersRouter);
 app.use("/products", ProductsRouter);
 app.use("/categories", CategoriesRouter);
