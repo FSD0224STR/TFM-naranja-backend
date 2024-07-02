16d0cc91e6490eea01e97d6b5636724188f9d89c
David Aranda <david.aranda@gmail.com>
2024-07-02 19:55:58 +0200

    Implementando logica de desconexion y añadida logica para conectar un admin automatica a una sala

--- Websockets/Socket.js
+++ Websockets/Socket.js
@@ -1,17 +1,22 @@
-const activeRooms = [];
+const { v4: uuidv4 } = require("uuid");
+
+let activeRooms = [];
+let activeAdmins = [];
 
 const configSocket = (io) => {
   io.on("connection", (socket) => {
-    // socket.on("userConnect", (user) => {
-    //   io.emit("userConnect", { msg: `El usuario ${user} se ha conectado` });
-    // });
-
     socket.on("joinPrivateRoom", () => {
-      const room = socket.id;
+      const room = uuidv4();
       socket.join(room);
       activeRooms.push(room);
       console.log(`Usuario ${socket.id} se ha unido a la sala ${room}`);
       socket.emit("roomJoined", room);
+
+      if (activeAdmins.length > 0) {
+        activeAdmins.forEach((adminId) => {
+          io.to(adminId).emit("adminJoinRoom", room);
+        });
+      }
     });
 
     socket.on("adminJoinRandomRoom", () => {
@@ -19,8 +24,9 @@ const configSocket = (io) => {
         const randomRoom =
           activeRooms[Math.floor(Math.random() * activeRooms.length)];
         socket.join(randomRoom);
+        activeAdmins.push(socket.id);
         console.log(
-          `Administrador ${socket.id} se ha unido a la sala ${randomRoom}`
+          `Administrador ${socket.id} se ha unido a la sala RANDOM ${randomRoom}`
         );
 
         socket.emit("adminRoomJoined", randomRoom);
@@ -28,6 +34,7 @@ const configSocket = (io) => {
           msg: `Administrador Conectado`,
         });
       } else {
+        activeAdmins.push(socket.id);
         console.log("No hay salas activas disponibles");
         socket.emit("adminRoomJoined", {
           msg: "No hay salas activas disponibles",
@@ -35,33 +42,68 @@ const configSocket = (io) => {
       }
     });
 
-    socket.on("userDisconnect", (user) => {
-      io.emit("userDisconnect", {
-        msg: `Usuario desconectado`,
-      });
-    });
+    socket.on("adminJoinRoom", (room) => {
+      socket.join(room);
+      console.log(
+        `Administrador ${socket.id} se ha unido a la nueva sala ${room}`
+      );
 
-    socket.on("message", (msg) => {
-      io.emit("message", msg);
+      socket.emit("adminRoomJoined", room);
+      io.to(room).emit("adminConnect", {
+        msg: `Administrador Conectado`,
+      });
+
+      // Añadir el admin a la sala
+      const roomIndex = activeRooms.findIndex((r) => r.room === room);
+      if (roomIndex !== -1) {
+        activeRooms[roomIndex].users.push(socket.id);
+      }
     });
 
     socket.on("privateMessage", ({ room, msg, typeUser }) => {
       io.to(room).emit("privateMessage", msg, typeUser);
     });
 
+    socket.on("userDisconnect", ({ room, typeUser }) => {
+      if (typeUser === "Admin") {
+        console.log(`Administrador ${socket.id} ha salida de la sala ${room}`);
+        io.to(room).emit("userDisconnect", {
+          msg: "Administrador desconectado",
+          typeUser,
+        });
+      } else {
+        console.log(
+          `Usuario ${socket.id} desconectado. La sala ${room} será cerrada.`
+        );
+        io.to(room).emit("userDisconnect", {
+          msg: "Usuario desconectado. La sala será cerrada.",
+          typeUser,
+        });
+        activeRooms = activeRooms.filter((r) => r !== room);
+      }
+      console.log("activeRooms: ", activeRooms);
+    });
+
     socket.on("disconnect", () => {
       console.log("Cliente desconectado");
-      socket.rooms.forEach((room) => {
-        if (room !== socket.id) {
-          const roomCount = activeRooms.get(room);
-          if (roomCount === 1) {
-            activeRooms.delete(room);
-          } else {
-            activeRooms.set(room, roomCount - 1);
-          }
+
+      activeAdmins = activeAdmins.filter((adminId) => adminId !== socket.id);
+
+      activeRooms = activeRooms.filter((room) => {
+        room.users = room.users.filter((userId) => {
+          console.log("userId: ", userId);
+          console.log("socket.id: ", socket.id);
+          return userId !== socket.id;
+        });
+        if (room.users.length === 0) {
+          console.log(`La sala ${room.room} ha sido eliminada`);
+          return false;
         }
+        return true;
       });
-      console.log("activeRooms: ", Array.from(activeRooms.keys()));
+
+      console.log("activeRooms: ", activeRooms);
+      console.log("activeAdmins: ", activeAdmins);
     });
   });
 };
